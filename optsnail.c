6057d1c6676718d2747f571c885442534d01a608
JIYUN1 <38645388+JIYUN1@users.noreply.github.com>
2018-04-27 17:31:22 +0900

    Update optsnail.c

--- optsnail.c
+++ optsnail.c
@@ -1,24 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 
 int main()
 {
 
-  int size;
-  int sum = 0;
-  int x,y,i,j;
-  int sw = 1;
+	int size;
+	int sum = 0;
+	int x, y, i, j;
+	int sw = 1;
 
-  int ** snail;
-
-  x = y = 0;
-
-  printf("input of snail size: ");
-  scanf("%d", &size);
-
-  snail = (int **)malloc(size*sizeof(int));
+  int **snail;
   
-  for(i=0; i<size; i++)
-    {
-      snail[
-    }
+	x = y = 0;
+
+	printf("input of snail size: ");
+	scanf("%d", &size);
+
+	snail = (int **)malloc(size*sizeof(int));
+
+
+	for(i = 0; i<size; i++)
+	{
+		snail[i] = (int *)malloc(size*sizeof(int));
+		
+    for(j=0; j < size; j++){
+     	snail[i][j] = 0;
+		}
+
+	}
+
+
+	while(sum != size*size)
+
+	{
+		for(i=0; i<size; i++)
+		{
+			snail[y][x] = sum;
+     	x += sw;
+    	sum += 1;
+		}
+
+		x -= sw;
+		y += sw;
+		size--;
+
+	
+		for(i = 0; i<size; i++)
+		{
+			snail[y][x] = sum;
+			y += sw;
+			sum += 1;
+		}
+		y -= sw;
+		sw *= -1;
+		x += sw;
+	}
+
+	//print
+
+	for(x=0; x<5; x++)
+	{
+		for(y=0; y<5; y++)
+		{
+			printf("%3d ", snail[x][y]);
+		}
+		printf("\n");
+	}
+
+}
+
